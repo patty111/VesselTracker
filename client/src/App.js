ada73b277f9cc1eb07f9b943d5356b63f6a503fd
Hsing-Tzu <daisyko1234@gmail.com>
2023-12-15 00:00:34 +0800

    add data to database

--- client/src/App.js
+++ client/src/App.js
@@ -11,27 +11,37 @@ const getRandomColor = () => {
   return color;
 };
 
-const World = ({ traceResponseData }) => {
+const World = () => {
   const [vesselPaths, setVesselPaths] = useState([]);
 
   useEffect(() => {
-    if (traceResponseData && traceResponseData.length > 0) {
-      let vesselPaths = [];
-      traceResponseData.forEach((featureCollection) => {
-        featureCollection.features.forEach(({ geometry, properties }) => {
-          geometry.coordinates.forEach((coords) => {
-            const randomColor = getRandomColor();
-            vesselPaths.push({ coords, vessel: { properties, color: randomColor } });
+    fetch('http://localhost:3012/api/getVesselTraceData')
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then((traceResponseData) => {
+        if (traceResponseData && traceResponseData.length > 0) {
+          let paths = [];
+          traceResponseData.forEach((featureCollection) => {
+            featureCollection.features.forEach(({ geometry, properties }) => {
+              geometry.coordinates.forEach((coords) => {
+                const randomColor = getRandomColor();
+                paths.push({ coords, vessel: { properties, color: randomColor } });
+              });
+            });
           });
-        });
-      });
 
-      setVesselPaths(vesselPaths);
-      console.log('Vessel trace data processed:', vesselPaths);
-    } else {
-      console.error('Invalid or missing features array in trace data:', traceResponseData);
-    }
-  }, [traceResponseData]);
+          setVesselPaths(paths);
+          console.log('Vessel trace data processed:', paths);
+        } else {
+          console.error('Invalid or missing features array in trace data:', traceResponseData);
+        }
+      })
+      .catch((error) => console.error('Error loading trace data:', error));
+  }, []);
 
   return (
     <Globe
@@ -40,10 +50,10 @@ const World = ({ traceResponseData }) => {
       backgroundImageUrl="//unpkg.com/three-globe/example/img/night-sky.png"
       pathsData={vesselPaths}
       pathPoints="coords"
-      pathPointLat={p => p[1]}
-      pathPointLng={p => p[0]}
-      pathColor={path => path.vessel.color}
-      pathLabel={path => path.vessel.properties.vessel.imo + "\n" + path.vessel.properties.vessel.name}
+      pathPointLat={(p) => p[1]}
+      pathPointLng={(p) => p[0]}
+      pathColor={(path) => path.vessel.color}
+      pathLabel={(path) => path.vessel.properties.vessel.imo + "\n" + path.vessel.properties.vessel.name}
       pathDashLength={0.1}
       pathDashGap={0.008}
       pathDashAnimateTime={12000}
--- server/index.js
+++ server/index.js
@@ -1,28 +1,24 @@
-// use express
 const express = require('express');
 const cors = require('cors');
 const Knex = require('knex');
 const { Model } = require('objection');
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json');
-
 const vesselRouter = require('./router/vesselRouter');
 const sdk = require('api')('@searoutes-docs/v2.0#14s2xo1blp0yncgo');
 const fs = require('fs');
 const path = require('path');
-
+const mongoose = require('mongoose');
 const Vessel = require('./model/Vessel');
 
 const knex = Knex({
-    client: 'sqlite3',
-    useNullAsDefault: true,
-    connection: {
-        filename: '../vessel.sqlite'
-    }
+  client: 'sqlite3',
+  useNullAsDefault: true,
+  connection: {
+    filename: '../vessel.sqlite',
+  },
 });
 
-
-// Add your actual API keys
 const apiKeys = ['HtO3NcFc9Oghha0HhOAFaHIloSd13As4DQDIDrU3', 'bvqAXYx7Vj2PEAbSHiqx26W9zRcauglS6tVxernf', 'TOpJLhrhIz22rl8CcveyKahvTKye3AwJwV8QMgwf'];
 let currentApiKeyIndex = 0;
 
@@ -31,70 +27,84 @@ const getNextApiKey = () => {
   return apiKeys[currentApiKeyIndex];
 };
 
+// Connect to MongoDB
+mongoose.connect('mongodb+srv://41071105H:41071105H@cluster0.h9q2tfk.mongodb.net/VesselTracker', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+  console.log('Connected to MongoDB');
+});
+
+// Define the VesselTrace model only once
+const VesselTraceModel = mongoose.model('routes', new mongoose.Schema({}, { strict: false }));
+
 const fetchVesselEtaFromDb = async () => {
-    try {
-      // Fetch all IMO numbers from the database
-      const results = await Vessel.query().select('IMO');
-  
-      // Filter out undefined values
-      const imos = results.filter(result => result.IMO).map(result => result.IMO);
-  
-      if (imos.length > 0) {
-        // Process each IMO number
-        for (const imo of imos) {
-          let retryCount = 0;
-          let success = false;
-  
-          while (!success && retryCount < 3) {
-            try {
-              const apiKey = getNextApiKey();
-              sdk.auth(apiKey);
-  
-              const traceResponse = await sdk.getVesselTrace({
-                imo,
-                departureDateTime: '2023-11-23T15:00:00Z',
-                arrivalDateTime: '2023-11-30T16:00:00Z',
-              });
-  
-              // Log or process the response as needed
-              console.log(`Vessel trace for IMO ${imo} using API key ${apiKey}:`, traceResponse.data);
-  
-              // Write data to a JSON file with a comma after each entry
-              const outputFilePath = path.join(__dirname, 'vessel_trace_data.json');
-              const entryJson = JSON.stringify(traceResponse.data, null, 2) + ',';
-              fs.writeFileSync(outputFilePath, entryJson, { flag: 'a' });
-  
-              console.log(`Vessel trace data written to vessel_trace_data.json for IMO ${imo}.`);
+  try {
+    const results = await Vessel.query().select('IMO');
+    const imos = results.filter(result => result.IMO).map(result => result.IMO);
+
+    if (imos.length > 0) {
+      for (const imo of imos) {
+        let retryCount = 0;
+        let success = false;
+
+        while (!success && retryCount < 3) {
+          try {
+            const apiKey = getNextApiKey();
+            sdk.auth(apiKey);
+
+            const traceResponse = await sdk.getVesselTrace({
+              imo,
+              departureDateTime: '2023-11-23T15:00:00Z',
+              arrivalDateTime: '2023-11-30T16:00:00Z',
+            });
+
+            console.log(`Vessel trace for IMO ${imo} using API key ${apiKey}:`, traceResponse.data);
+
+            // Store trace data in MongoDB
+            const vesselTraceData = new VesselTraceModel(traceResponse.data);
+            await vesselTraceData.save();
+
+            // Store trace data in JSON file
+            const outputFilePath = path.join(__dirname, 'vessel_trace_data.json');
+            const entryJson = JSON.stringify(traceResponse.data, null, 2) + ',';
+            fs.writeFileSync(outputFilePath, entryJson, { flag: 'a' });
+
+            console.log(`Vessel trace data written to MongoDB and vessel_trace_data.json for IMO ${imo}.`);
+            success = true;
+          } catch (error) {
+            if (error.status === 404) {
+              console.warn(`Vessel trace not found for IMO ${imo}. Proceeding to the next IMO.`);
               success = true;
-            } catch (error) {
-              if (error.status === 404) {
-                console.warn(`Vessel trace not found for IMO ${imo}. Proceeding to the next IMO.`);
-                success = true;  // Consider it a success to proceed to the next iteration
-              } else if (error.status === 429) {
-                const retryDelay = Math.pow(2, retryCount) * 1000;
-                console.warn(`Rate limited. Retrying in ${retryDelay} milliseconds...`);
-                await new Promise(resolve => setTimeout(resolve, retryDelay));
-                retryCount++;
-              } else {
-                throw error;  // Re-throw other errors
-              }
+            } else if (error.status === 429) {
+              const retryDelay = Math.pow(2, retryCount) * 1000;
+              console.warn(`Rate limited. Retrying in ${retryDelay} milliseconds...`);
+              await new Promise(resolve => setTimeout(resolve, retryDelay));
+              retryCount++;
+            } else {
+              throw error;
             }
           }
-  
-          if (!success) {
-            console.error(`Failed to fetch vessel trace for IMO ${imo} after multiple retries.`);
-          }
         }
-  
-        console.log('Vessel trace data processing complete.');
-      } else {
-        console.log('No valid IMO numbers found in the database.');
+
+        if (!success) {
+          console.error(`Failed to fetch vessel trace for IMO ${imo} after multiple retries.`);
+        }
       }
-    } catch (error) {
-      console.error('Error fetching or writing vessel trace data:', error);
+
+      console.log('Vessel trace data processing complete.');
+    } else {
+      console.log('No valid IMO numbers found in the database.');
     }
+  } catch (error) {
+    console.error('Error fetching or writing vessel trace data:', error);
+  }
 };
-  
+
 Model.knex(knex);
 
 const app = express();
@@ -105,7 +115,6 @@ app.use(express.json());
 app.use('/vessel', vesselRouter);
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-// Endpoint to trigger the vessel trace data fetch
 app.get('/fetchVesselTrace', async (req, res) => {
   console.log('Fetching vessel trace data...');
   await fetchVesselEtaFromDb();
@@ -113,7 +122,17 @@ app.get('/fetchVesselTrace', async (req, res) => {
   // Delay for 1 minute before allowing the next request
   setTimeout(() => {
     res.send('Vessel trace data fetch initiated.');
-  }, 60000); // 60000 milliseconds = 1 minute
+  }, 60000);
+});
+
+app.get('/api/getVesselTraceData', async (req, res) => {
+  try {
+    const traceData = await VesselTraceModel.find();
+    res.json(traceData);
+  } catch (error) {
+    console.error('Error fetching vessel trace data from MongoDB:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 });
 
 app.listen(port, () => {
