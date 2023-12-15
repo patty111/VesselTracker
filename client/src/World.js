4fea4dda8d6e981695dcc6200a7240292231bfe8
Hsing-Tzu <daisyko1234@gmail.com>
2023-12-15 23:59:11 +0800

    add the database connection to server

--- client/src/World.js
+++ client/src/World.js
@@ -18,74 +18,85 @@ const World = ({ traceResponseData }) => {
   const [selectedPath, setSelectedPath] = useState(null);
 
   useEffect(() => {
-    if (traceResponseData && traceResponseData.length > 0) {
-      let vesselPaths = [];
-      traceResponseData.forEach((featureCollection) => {
-        featureCollection.features.forEach(({ geometry, properties }) => {
-          geometry.coordinates.forEach((coords) => {
-            const randomColor = getRandomColor();
-            vesselPaths.push({ coords, vessel: { properties, color: randomColor } });
+    fetch('http://localhost:3012/api/VesselTraceData')
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then((traceResponseData) => {
+        if (traceResponseData && traceResponseData.length > 0) {
+          let vesselPaths = [];
+          traceResponseData.forEach((featureCollection) => {
+            featureCollection.features.forEach(({ geometry, properties }) => {
+              geometry.coordinates.forEach((coords) => {
+                const randomColor = getRandomColor();
+                vesselPaths.push({ coords, vessel: { properties, color: randomColor } });
+              });
+            });
           });
-        });
-      });
 
-      setVesselPaths(vesselPaths);
-      console.log('Vessel trace data processed:', vesselPaths);
-    } else {
-      console.error('Invalid or missing features array in trace data:', traceResponseData);
-    }
-  }, [traceResponseData]);
+          setVesselPaths(vesselPaths);
+          console.log('Vessel trace data processed:', vesselPaths);
+        } else {
+          console.error('Invalid or missing features array in trace data:', traceResponseData);
+        }
+      })
+      .catch((error) => console.error('Error loading trace data:', error));
+  }, []);
 
   return (
     <>
-    <Globe
-      globeImageUrl="//unpkg.com/three-globe/example/img/earth-dark.jpg"
-      bumpImageUrl="//unpkg.com/three-globe/example/img/earth-topology.png"
-      backgroundImageUrl="//unpkg.com/three-globe/example/img/night-sky.png"
-      pathsData={vesselPaths}
-      pathPoints="coords"
-      pathPointLat={p => p[1]}
-      pathPointLng={p => p[0]}
-      pathColor={path => path.vessel.color}
-      pathLabel={path => path.vessel.properties.vessel.imo + "\n" + path.vessel.properties.vessel.name}
-      pathDashLength={0.1}
-      pathDashGap={0.008}
-      pathStroke={4}
-      pathDashAnimateTime={12000}
-      onPathClick={(path) => {
-        setSelectedPath(path);
-        console.log(path);
-      }}
-    />
+      <Globe
+        globeImageUrl="//unpkg.com/three-globe/example/img/earth-dark.jpg"
+        bumpImageUrl="//unpkg.com/three-globe/example/img/earth-topology.png"
+        backgroundImageUrl="//unpkg.com/three-globe/example/img/night-sky.png"
+        pathsData={vesselPaths}
+        pathPoints="coords"
+        pathPointLat={p => p[1]}
+        pathPointLng={p => p[0]}
+        pathColor={path => path.vessel.color}
+        pathLabel={path => path.vessel.properties.vessel.imo + "\n" + path.vessel.properties.vessel.name}
+        pathDashLength={0.1}
+        pathDashGap={0.008}
+        pathStroke={4}
+        pathDashAnimateTime={12000}
+        onPathClick={(path) => {
+          setSelectedPath(path);
+          console.log(path);
+        }}
+      />
 
-    {selectedPath && (
-      <div class="gradient-cards">
-        <div class="card">
-          <div class="container-card" style={{
-            background: `linear-gradient(71deg, #0d1212, ${chroma(selectedPath.vessel.color).darken().desaturate(0).hex()}, #0d1212)`}}
+      {selectedPath && (
+        <div className="gradient-cards">
+          <div className="card">
+            <div className="container-card" style={{
+              background: `linear-gradient(71deg, #0d1212, ${chroma(selectedPath.vessel.color).darken().desaturate(0).hex()}, #0d1212)`
+            }}
             >
 
-            <p class="card-title">{selectedPath.vessel.properties.vessel.name}</p>
-            <p class="card-description">IMO: {selectedPath.vessel.properties.vessel.imo}</p>
-            <p class="card-description">{(() => {
-              try {
-                var coord_arr = selectedPath.vessel.properties.areas.features[0].geometry.coordinates;
-                return `lat, lon:  
+              <p className="card-title">{selectedPath.vessel.properties.vessel.name}</p>
+              <p className="card-description">IMO: {selectedPath.vessel.properties.vessel.imo}</p>
+              <p className="card-description">{(() => {
+                try {
+                  var coord_arr = selectedPath.vessel.properties.areas.features[0].geometry.coordinates;
+                  return `lat, lon:  
                   ${coord_arr[1].toFixed(2)}, ${coord_arr[0].toFixed(2)}
                   `;
-              }  catch (error) {
-                return "No area data available";
+                } catch (error) {
+                  return "No area data available";
+                }
               }
-            }
-            )()}</p>
-            <p class="card-description">Speed(Knots): {selectedPath.vessel.properties.speedInKts}</p>
-            <p class="card-description">draft(吃水深度): {selectedPath.vessel.properties.vessel.draft}</p>
-            <p class="card-description">length: {selectedPath.vessel.properties.vessel.length}</p>
-            <p class="card-description">width: {selectedPath.vessel.properties.vessel.width}</p>
+              )()}</p>
+              <p className="card-description">Speed(Knots): {selectedPath.vessel.properties.speedInKts}</p>
+              <p className="card-description">draft(吃水深度): {selectedPath.vessel.properties.vessel.draft}</p>
+              <p className="card-description">length: {selectedPath.vessel.properties.vessel.length}</p>
+              <p className="card-description">width: {selectedPath.vessel.properties.vessel.width}</p>
+            </div>
           </div>
         </div>
-      </div>
-    )}
+      )}
     </>
   );
 };
